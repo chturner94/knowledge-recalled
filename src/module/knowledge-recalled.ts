22dcff83c25c36a556f143f8f0544a510dad3021
Alec Holloway <rh1397@msstate.edu>
2022-12-06 04:41:08 -0500

    test file and adjustmentes to gmClass

--- src/module/knowledge-recalled.ts
+++ src/module/knowledge-recalled.ts
@@ -25,50 +25,60 @@
 import { registerSettings } from './settings';
 import { preloadTemplates } from './preloadTemplates';
 import {array, boolean, string} from 'yargs';
+import testObject from './tests.json';
 
 // Initialize module
 Hooks.once('init', async () => {
   console.log('knowledge-recalled | Initializing knowledge-recalled');
 
-  type actorDescriptionStructure = {
-      value: string | string[];
-      isVisibleForPlayer: boolean;
-  }
+    type actorDescriptionStructure = {
+        value: string | string[];
+        isVisibleForPlayer: boolean;
+    }
 
-  type statStructure = {
-      value?: number;
-      beforeDC?: number;
-      afterDC?:  number;
-      isVisibleForPlayer: boolean;
-  }
+    type abilities = actorDescriptionStructure
+
+    type statStructure = {
+        value?: number;
+        beforeDC?: number;
+        afterDC?:  number;
+        isVisibleForPlayer: boolean;
+    }
+
+    type objectStructure = {
+        value: object;
+        isVisibleForPlayer: boolean;
+    }
 
 
-  interface ActorDescription {
-      name: actorDescriptionStructure;
-      actorId: actorDescriptionStructure;
-      description: actorDescriptionStructure;
-      profileImg: actorDescriptionStructure;
-      rarity: actorDescriptionStructure;
-      traits: actorDescriptionStructure;
-  }
-  interface Stats {
-      ac: statStructure;
-      fortitude: statStructure;
-      reflex: statStructure;
-      will: object; //all the others were type number. So check this attribute in foundry
-      immunities?: statStructure;
-      resistance?: statStructure
-      weakness?: statStructure;
+    interface ActorDescription {
+        name: actorDescriptionStructure;
+        actorId: actorDescriptionStructure;
+        description: actorDescriptionStructure;
+        profileImg: actorDescriptionStructure;
+        rarity: actorDescriptionStructure;
+        traits: actorDescriptionStructure;
+    }
 
-  }
-  interface Abilities {
-      attacks: string[];
-      abilities?: string[];
-      passives?: string[];
-      spells?: string[];
-  }
+    interface Stats {
+        ac: statStructure;
+        fortitude: statStructure;
+        reflex: statStructure;
+        will: objectStructure;
+        immunities?: objectStructure;
+        resistance?: objectStructure;
+        weakness?: objectStructure;
+    }
 
-  class actorTemplate implements ActorDescription, Stats, Abilities {
+
+    interface Abilities {
+        attacks: abilities;
+        abilities?: abilities;
+        passives?: abilities;
+        spells?: abilities;
+    }
+
+    class actorTemplate implements ActorDescription, Stats, Abilities {
 
         name: actorDescriptionStructure;
         actorId: actorDescriptionStructure;
@@ -79,94 +89,100 @@ Hooks.once('init', async () => {
         ac: statStructure;
         fortitude: statStructure;
         reflex: statStructure;
-        will: Object;
-        immunities: statStructure;
-        resistance: statStructure
-        weakness: statStructure;
-        //attacks: string[];
-       /// passiveAbilities: string[];
+        will: objectStructure;
+        immunities: objectStructure;
+        resistance: objectStructure
+        weakness: objectStructure;
+        attacks: abilities;
+        /// passiveAbilities: string[];
         //spells: string[];
 
 
         constructor(actorId: string, actor: object) {
-        //the properties can be found inside ui.combat.combats[arrayId].turns[arrayId]
-        // actorId is at the very top of this and everything else is nested inside of the
-        // actor object.
+            //the properties can be found inside ui.combat.combats[arrayId].turns[arrayId]
+            // actorId is at the very top of this and everything else is nested inside of the
+            // actor object.
             this.name =
                 {
-                    value = actor.name;
-                    isVisibleForPlayer = false;
+                    value: actor.name,
+                    isVisibleForPlayer: false
                 };
             this.actorId =
                 {
-                    value = actorId;
-                    isVisibleForPlayer = false;
+                    value: actorId,
+                    isVisibleForPlayer: false
                 };
             this.description =
                 {
-                    value = actor.system.details.publicNotes;
-                    isVisibleForPlayer = false;
+                    value: actor.system.details.publicNotes,
+                    isVisibleForPlayer: false
                 };
             this.profileImg =
                 {
-                    value = actor.img;
-                    isVisibleForPlayer = false;
+                    value: actor.img,
+                    isVisibleForPlayer: false
                 };
             this.rarity =
                 {
-                    value = actor.system.traits.rarity;
-                    isVisibleForPlayer = false;
+                    value: actor.system.traits.rarity,
+                    isVisibleForPlayer: false
                 };
             this.traits =
                 {
-                    value = actor.system.traits.value;
-                    isVisibleForPlayer = false;
+                    value: actor.system.traits.value,
+                    isVisibleForPlayer: false
                 };
-            this.ac =
-                {
-                    value = actor.system.attributes.ac.value;
-                    //beforeDC
-                    //afterDC
-                    isVisibleForPlayer = false
-                };
-            this.fortitude =
-                {
-                   value = actor.system.saves.fortitude;
-                   //beforeDC
-                    //afterDC
-                    isVisibleForPlayer = false
-                }
-            this.reflex =
-                {
-                    value = actor.system.saves.reflex;
-                    //beforeDC;
-                    //afterDc
-                    isVisibleForPlayer= false
-                };
-            this.will = actor.system.saves.will;
 
 
             this.immunities =
                 {
-                    value = actor.system.traits.immunity;
-                    //beforeDC;
-                    //afterDc
-                    isVisibleForPlayer= false
+                    value: actor.system.traits.di.value,
+                    isVisibleForPlayer: false
                 };
             this.resistance =
                 {
-                    value = actor.system.traits.resistance;
-                    //beforeDC;
-                    //afterDc
-                    isVisibleForPlayer= false;
+                    value: actor.system.traits.dr,
+                    isVisibleForPlayer: false
                 };
             this.weakness =
                 {
-                    value = actor.system.traits.weakness;
+                    value: actor.system.traits.dv,
+                    isVisibleForPlayer: false
+                };
+
+            this.ac =
+                {
+                    value: actor.system.attributes.ac.value,
+                    //beforeDC
+                    //afterDC
+                    //beforeDC
+                    //afterDC
+                    isVisibleForPlayer: false
+                };
+            this.fortitude =
+                {
+                    value: actor.system.saves.fortitude.value,
+                    //beforeDC
+                    //afterDC
+                    isVisibleForPlayer: false
+                }
+            this.reflex =
+                {
+                    value: actor.system.saves.reflex.value,
                     //beforeDC;
                     //afterDc
-                    isVisibleForPlayer= false;
+                    isVisibleForPlayer: false
                 };
+            this.will =
+                {
+                    //beforeDC
+                    //afterDC
+                    value: actor.system.saves.will.value,
+                    isVisibleForPlayer: false
+                }
+
+
+
             //this.attacks =  actor.system.actions[0].slug;
             //this.passiveAbilities = actor.passiveAbilities
             //this.spells = actor.spells
@@ -174,59 +190,60 @@ Hooks.once('init', async () => {
 
         }
 
-  }
-  class GMCombatKnowledgeJournal {        //PASS IN UI
-      actorObjectArray: object[] = [];
-      ui: object;
+    }
+    class GMCombatKnowledgeJournal {        //PASS IN UI
+        actors: object[] = [];
+        ui: object;
 
-      constructor(ui: object) {
-          this.ui = ui;
-      }
+        constructor(ui: object) {
+            this.ui = ui;
+        }
 
-      getActorsFromTheCurrentEncounter(): void {
-          let encounters = ui.combat.combats;
-          for (let encountersCounter = 0; encountersCounter < encounters.length; encountersCounter++) {
-              if (encounters[encountersCounter].isActive) {
-                  this.addActorsToActorObjectArray(encounters[encountersCounter])
-              } else {
-                  console.log('not active; skipping')
-              }
+        getActorsFromEncounters(ui:object): void {
+            let encounters = ui.combat.combats;
+            for (let encountersCounter = 0; encountersCounter < encounters.length; encountersCounter++) {
+                if (encounters[encountersCounter].isActive) {
+                    this.addActorsToActorObjectArray(encounters[encountersCounter])
+                } else {
+                    console.log('not active; skipping')
+                }
 
-          }
-      }
-      addActorsToActorObjectArray(encounter: object) {
-          let combatants = encounter.turns;
-          for (let combatantsCounter = 0; combatantsCounter < combatants.length; combatantsCounter++) {
-              if (combatants[combatantsCounter].isNPC) {
-                  let combatant = combatants[combatantsCounter];
-                  let actorId = combatants[combatantsCounter].actorId;
-                  let actor = combatant.actor;
+            }
+        }
+        addActorsToActorObjectArray(encounter: object) {
+            let combatants = encounter.turns;
+            for (let combatantsCounter = 0; combatantsCounter < combatants.length; combatantsCounter++) {
+                if (combatants[combatantsCounter].isNPC) {
+                    let combatant = combatants[combatantsCounter];
+                    let actorId = combatants[combatantsCounter].actorId;
+                    let actor = combatant.actor;
 
-                  if(this.isDuplicate(actorId)) {
-                      console.log("Duplicated actor")
-                  }
-                  else{
-                      let actorObject = new actorTemplate(actorId, actor);
-                      this.actorObjectArray.push(actorObject);
-                  }
+                    if(this.isDuplicate(actorId)) {
+                        console.log("Duplicated actor")
+                    }
+                    else{
+                        let actorObject = new actorTemplate(actorId, actor);
+                        this.actors.push(actorObject);
+                    }
 
-                  //jsonify actorObjectArray
-              } else {
-                  console.log('not an npc; skipping')
-              }
-          }
-      }
-      isDuplicate(actorId: string): boolean {
-          for(let actorCounter = 0; actorCounter <= this.actorObjectArray.length; actorCounter++) {
-              if(actorId == this.actorObjectArray[actorCounter].actorId) {
-                  return true
-              }
-          }
-          return false
-      }
-  }
+                    //jsonify actorObjectArray
+                } else {
+                    console.log('not an npc; skipping')
+                }
+            }
+        }
+        isDuplicate(actorId: string): boolean {
+            for(let actorCounter = 0; actorCounter <= this.actors.length; actorCounter++) {
+                if(actorId == this.actors[actorCounter].actorId) {
+                    return true
+                }
+            }
+            return false
+        }
+    }
+
   class PlayerJournal implements GMCombatKnowledgeJournal {
-      actorObjectArray: object[] = [];
+      actors: object[] = [];
      // set
 
 
@@ -245,19 +262,21 @@ Hooks.once('setup', async () => {
   // ready
     let GMJournal = new GMCombatKnowledgeJournal(ui)
     //let PlayerJournal = new PlayerJournal
+    //PlayerJournal = GMJournal    ---- How to check for visibility? Would it be on the front end?
+
 });
 
 // When ready
 Hooks.once('ready', async () => {
   // Do anything once the module is ready
-    //encounterChecker()
-        //GmJournalObject.getActorsFromTheCurrentEncounter()
-        //GMJournalObject.actorObjectArray
-        //if GMJournalObject is updated
-        //  playerJournal.actorObjectArray = gmJournalObject.actorObjectArray but ONLY SHOW ICON?
-        //if a DC is passed isVisible is set to true this value becomes visible to the players
-        //push data to frontend
+
+    //Open GM Journal
+        //GmJournal.getActorsEncounters() this stores the actors in GMJournal.actors
+        //click on a particular actor in the gm journal
+        //roll DC check
+        //if DC check is passed make values visible
+
+
 
 }
-
 
